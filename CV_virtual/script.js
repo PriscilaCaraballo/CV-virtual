376fa93af64d6f91244b4ae318f8b7989bfc350a
PriscilaCaraballo <priscilatechera2@gmail.com>
2025-07-04 23:39:06 -0300

    Primer commit de mi proyecto CV

---
+++ CV_virtual/script.js
@@ -0,0 +1,43 @@
+function mostrarMas() {
+  const parrafo = document.getElementById("extra-info");
+  const textoBoton = document.getElementById("toggleText");
+
+  if (parrafo.style.display === "none") {
+    parrafo.style.display = "block";
+    textoBoton.textContent = "Mostrar menos";
+  } else {
+    parrafo.style.display = "none";
+    textoBoton.textContent = "Mostrar más";
+  }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => 
+{
+    const foto = document.getElementById("foto-perfil");
+
+    foto.addEventListener("click", () => {
+        document.body.classList.toggle("modo-oscuro");
+    });
+});
+
+// Marcar el checkbox si estás en la página en inglés
+    window.addEventListener("DOMContentLoaded", () => {
+        if (location.href.includes("/English/")) {
+            document.getElementById("Idioma").checked = true;
+        }
+    });
+
+    // Cambiar entre páginas al cambiar el estado del checkbox
+    let cambioIdioma = document.getElementById("Idioma");
+
+    cambioIdioma.addEventListener("change", function () {
+        if (cambioIdioma.checked) {
+            // Ir a la versión en inglés
+            location.href = "../English/index.html";
+        } else {
+            // Ir a la versión en español
+            location.href = "../index.html";
+        }
+    });
